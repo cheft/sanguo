0d6d53699ae3c95f25d6b41588c7349eea172589
陈海峰 <cn.cheft@gmail.com>
2017-08-31 18:32:48 +0800

    重构选择君主势力代码

--- src/Game.js
+++ src/Game.js
@@ -41,95 +41,20 @@ function onShowMenu2() {
   Laya.stage.addChild(menu2);
 
   menu2.backBtn.on(Event.CLICK, menu2, onBack);
-  menu2.dznq.on(Event.CLICK, this, onShowSelectLord.bind(this, 'dznq'));
-  menu2.ccjq.on(Event.CLICK, this, onShowSelectLord.bind(this, 'ccjq'));
-  menu2.cbzz.on(Event.CLICK, this, onShowSelectLord.bind(this, 'cbzz'));
-  menu2.sgdl.on(Event.CLICK, this, onShowSelectLord.bind(this, 'sgdl'));
+
+  var dznqSelectLord = new SelectLord('dznq');
+  var ccjqSelectLord = new SelectLord('ccjq');
+  var cbzzSelectLord = new SelectLord('cbzz');
+  var sgdlSelectLord = new SelectLord('sgdl');
+
+  menu2.dznq.on(Event.CLICK, this, dznqSelectLord.open.bind(dznqSelectLord));
+  menu2.ccjq.on(Event.CLICK, this, ccjqSelectLord.open.bind(ccjqSelectLord));
+  menu2.cbzz.on(Event.CLICK, this, cbzzSelectLord.open.bind(cbzzSelectLord));
+  menu2.sgdl.on(Event.CLICK, this, sgdlSelectLord.open.bind(sgdlSelectLord));
 }
 
 function onBack() {
   Laya.stage.removeChild(this);
-}
-
-function onShowSelectLord(period) {
-  var selectLord = new selectLordUI();
-  data.curPeriod = period;
-
-  // 给选择界面的地图加阴影
-  var glowFilter = new Laya.GlowFilter('#000000', 10, 0, 0);
-  selectLord.whoMap.filters = [glowFilter];
-  Laya.stage.addChild(selectLord);
-  
-  selectLord.backBtn.on(Event.CLICK, selectLord, onBack);
-
-  selectLord.lordList.vScrollBarSkin='';
-
-  var arr =  data[period + 'Lord'];
-  var tmp = [];
-  for (var i = 0; i < arr.length; i++) {
-    tmp.push({name: arr[i]});
-  }
-  selectLord.lordList.array = tmp;
-  selectLord.lordList.renderHandler = new Handler(this, onListRender.bind(selectLord));
-
-  var headFilter = new Laya.GlowFilter('#000000', 5, 0, 0);  
-  selectLord.lordHead.filters = [headFilter];
-
-  
-}
-
-var lastSelectedLabel = null;
-function onListRender(cell, index) {
-  var label = cell.getChildByName('name');
-  if (index === 0) {
-    onLabelClick.call(this, 0, label);
-    lastSelectedLabel = label;
-  }
-  label.on(Event.CLICK, this, onLabelClick, [index, label]);
-}
-
-function onLabelClick(index, label) {
-  var name = label.text;
-  if (lastSelectedLabel) {
-    lastSelectedLabel.borderColor = '';
-  }
-  label.border = 3;
-  label.borderColor = '#131313';
-  lastSelectedLabel = label;
-  var citys = data[data.curPeriod + 'CityToWho'][index].split(' ');
-  var info = data[data.curPeriod + 'HerosInfo'][index].split(' ');
-  this.lordInfo.text = '年龄： ' + info[13] + '\n\n' + '武力：' + info[3] + '\n\n' + '智谋：' + info[4] + '\n\n' +
-  '将领：' + data[data.curPeriod + 'HerosToWho'][index].split(' ')[0] + '名\n\n' + '城池：' + citys.shift() + '座\n\n'
-  // '兵种：' + data.bingzhong[info[9]] + '\n\n' + '所驻城池：' + data.cityname[info[14]];
-  this.lordDesc.text = data.lordDesc[name];
-  flagCitysPosition(citys, this);
-
-  var picIndex = data[data.curPeriod + 'HerosName'].indexOf(name);
-  var row = Math.floor(picIndex/10); // 1 行 10 个头像，移动到第几行
-  var col = picIndex % 10; // 移动到第几列
-  var texture = Laya.loader.getRes('img/head_' + data.curPeriod + '.jpg');
-  var texEnd = Laya.Texture.create(texture,  col * 120, row * 120, 120, 120);
-  this.lordHead.graphics.drawTexture(texEnd, 0, 0, 120, 120);
-}
-
-
-var existSps = []; // 存储已画好的标记
-function flagCitysPosition(citys, selectLord) {
-  if (existSps.length) {
-    for (var i = 0; i < existSps.length; i++) {
-      selectLord.removeChild(existSps[i]);
-    }
-    existSps = [];
-  }
-  for (var i = 0; i < citys.length; i++) {
-    var sp = new Laya.Sprite();
-    selectLord.addChild(sp);
-    existSps.push(sp);
-    // 获取城池坐标，相对于地图算的
-    var pos = data.cityToWhoPosition[(citys[i]-1)].split(' ');
-    // 加 10 像素，是因为地图在这个 UI 上的 margin-left 和 margin-top 是 10
-    sp.graphics.drawRect(10 + parseInt(pos[0], 10), 10 + parseInt(pos[1], 10), 20, 20, '#000000');
-  }
 }
 
 function onShowMain() {
---
+++ src/SelectLord.js
@@ -0,0 +1,126 @@
+!(function(global) {
+  var Event = Laya.Event;
+  var Handler = Laya.Handler;
+
+  function SelectLord(period) {
+    this.period = period; // 四个时期：dznq, ccjq, cbzz, sgdl
+    this.lastSelectedLabel = null; // 最后选择的君主 Label
+    this.existSps = []; // 已存在的标记城池点
+    this.ui = new selectLordUI();
+    this.handleUI();
+    this.handleData();
+  }
+
+  SelectLord.prototype.open = function() {
+    Laya.stage.addChild(this.ui);
+  }
+
+  SelectLord.prototype.close = function() {
+    Laya.stage.removeChild(this.ui);
+  }
+
+  SelectLord.prototype.handleUI = function() {
+    // 给地图加阴影效果
+    var glowFilter = new Laya.GlowFilter('#000000', 10, 0, 0);
+    this.ui.whoMap.filters = [glowFilter];
+    // 给头像加阴影效果
+    var headFilter = new Laya.GlowFilter('#000000', 5, 0, 0);  
+    this.ui.lordHead.filters = [headFilter];
+
+    // 指定君主列表的的纵向滚动皮肤
+    this.ui.lordList.vScrollBarSkin = '';
+
+    // 绑定返回按钮事件
+    this.ui.backBtn.on(Event.CLICK, this, this.close);
+  }
+
+  SelectLord.prototype.handleData = function() {
+    var arr =  data[this.period + 'Lord'];
+    var tmp = [];
+    for (var i = 0; i < arr.length; i++) {
+      tmp.push({name: arr[i]});
+    }
+    this.ui.lordList.array = tmp;
+    this.ui.lordList.renderHandler = new Handler(this, this.lordListRender);
+
+    var headFilter = new Laya.GlowFilter('#000000', 5, 0, 0);  
+    this.ui.lordHead.filters = [headFilter];
+  }
+
+  SelectLord.prototype.lordListRender = function(cell, index) {
+    var label = cell.getChildByName('name');
+    if (index === 0) {
+      this.lordLabelClick(0, label);
+      this.lastSelectedLabel = label;
+    }
+    label.on(Event.CLICK, this, this.lordLabelClick, [index, label]);
+  }
+
+  SelectLord.prototype.lordLabelClick = function(index, label) {
+    // 将上次选中的 label 边框颜色还原，然后设置当前选中
+    if (this.lastSelectedLabel) this.lastSelectedLabel.borderColor = '';
+    label.borderColor = '#131313';
+    this.lastSelectedLabel = label;
+    
+    var name = label.text;
+    this.fillLordInfo(index, name);
+    this.fillHeadImage(name);
+  }
+
+  SelectLord.prototype.fillLordInfo = function(index, name) {
+    // 获取当前君主在这个时期拥有的城池，转化成数组，其中第1个数据为长度信息
+    var cities = data[this.period + 'CityToWho'][index].split(' ');
+    // 获取当前君主在这个时期拥有的将领，转化成数组，其中第1个数据为长度信息
+    var heros = data[this.period + 'HerosToWho'][index].split(' ');
+    // 获取当前君主在这个时期的属性信息
+    var info = data[this.period + 'HerosInfo'][index].split(' ');
+    // 展示当前君主的属性信息
+    this.ui.lordInfo.text = '年龄： ' + info[13] + '\n\n' + '武力：' + info[3] + '\n\n' + '智谋：' + info[4] + '\n\n' +
+      '将领：' + heros[0] + '名\n\n' + '城池：' + cities.shift() + '座\n\n'
+      // '兵种：' + data.bingzhong[info[9]] + '\n\n' + '所驻城池：' + data.cityname[info[14]];
+    
+    // 展示当前君主的介绍信息
+    this.ui.lordDesc.text = data.lordDesc[name];
+
+    // 此 cities 已在前面删除掉了第1个长度数据
+    this.flagCityMap(cities)
+  }
+
+  SelectLord.prototype.flagCityMap = function(cities) {
+    // 删掉上一次标记地图的点
+    if (this.existSps.length > 0) {
+      for (var i = 0; i < this.existSps.length; i++) {
+        this.ui.removeChild(this.existSps[i]);
+      }
+      this.existSps = [];
+    }
+    
+    // 重新标记当前君主所拥有的城池坐标信息
+    for (var i = 0; i < cities.length; i++) {
+      var sp = new Laya.Sprite();
+      this.ui.addChild(sp);
+      this.existSps.push(sp);
+      // 获取城池坐标，相对于地图算的
+      var pos = data.cityToWhoPosition[(cities[i]-1)].split(' ');
+      // 加 10 像素，是因为地图在这个 UI 上的 margin-left 和 margin-top 是 10，20 为宽度和高度
+      sp.graphics.drawRect(10 + parseInt(pos[0], 10), 10 + parseInt(pos[1], 10), 20, 20, '#000000');
+    }
+  }
+  
+  SelectLord.prototype.fillHeadImage = function(name) {
+    // 从所有将领列表中获取君主的索引
+    var imgIndex = data[this.period + 'HerosName'].indexOf(name);
+    // 因为头像是分不同时间，并且所有的头像在一起的
+    var texture = Laya.loader.getRes('img/head_' + this.period + '.jpg');
+    // 通过索引算出头像在第几行，头像从左往右，从上往下数的
+    var row = Math.floor(imgIndex / 10);
+    // 算出头像在第几列
+    var col = imgIndex % 10;
+    // 通过截取的方式获得纹理切图: x, y, width, height
+    var texEnd = Laya.Texture.create(texture,  col * 120, row * 120, 120, 120);
+    // 显示在头像上
+    this.ui.lordHead.graphics.drawTexture(texEnd, 0, 0, 120, 120);
+  }
+  
+  global.SelectLord = SelectLord;
+})(window);
